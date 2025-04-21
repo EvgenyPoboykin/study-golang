04b67895ea0f0c8694f028bf3faa604f8313cbc1
Evgeny Poboykin <poboykineg@bcs.ru>
2025-04-21 18:39:56 +0700

    feat(03_rest_api/baase_project): add base project

---
+++ 03_rest_api/base_project/cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"base/rest_api/di"
+)
+
+func main() {
+	di.NewContainer()
+}
---
+++ 03_rest_api/base_project/internal/validation/todos.go
@@ -0,0 +1,29 @@
+package validation
+
+import (
+	"base/rest_api/internal/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ValidateBodyCreateTodo(c *gin.Context) *models.CreateTodo {
+	body := models.CreateTodo{}
+	c.ShouldBindJSON(&body)
+
+	return &body
+}
+
+func ValidateArgsUpdateTodo(c *gin.Context) (string, *models.UpdateTodo) {
+	id := c.Param("id")
+
+	body := models.UpdateTodo{}
+	c.ShouldBindJSON(&body)
+
+	return id, &body
+}
+
+func ValidateParamDeleteTodo(c *gin.Context) string {
+	id := c.Param("id")
+
+	return id
+}
